9666ea7936b898b254925821d41c20aed0ae5b77
Jesus Picardo <jesuspicardo95@gmail.com>
2024-11-01 23:03:54 +0000

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# cosmos_evaluacion
+# Evaluacion de curso Cosmos DB
+
+## Pasos para evaluar la aplicacion en el repositorio
+1. Abrir el codespace asociado al repositorio
+2. Una vez dentro del codespace lanzar el comando siguiente en la seccion de TERMINAL
+```
+uvicorn app:app --reload
+```
+3. Al terminar de levantar la aplicacion en la seccion PUERTOS permitira ingresar a una URL ubicada en la columna Direccion reenviada haciendo Ctrl + Click en la direccion
+4. Una vez abierta la direccion agregarle al final de la URL lo siguiente : /docs
+5. Al abrirse la pagina con todas los endpoints expuestos en el API se podran probar uno a uno con la opcion de Try Out y Execute
--- app.py
+++ app.py
@@ -49,3 +49,55 @@ def delete_usuario(usuario_id: str):
         raise HTTPException(status_code=404, detail='Usuario no encontrado')
     except exceptions.CosmosHttpResponseError as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+###PROYECTOS###
+#Crear proyecto POST
+@app.post("/proyectos/",response_model=Proyecto, status_code=201)
+def add_proyecto(proyecto: Proyecto):
+    try:
+        usuario = container_usuario.read_item(item=proyecto.id_usuario, partition_key=proyecto.id_usuario)
+        container_proyecto.create_item(body=proyecto.dict())
+        return proyecto
+    except exceptions.CosmosResourceNotFoundError:
+        raise HTTPException(status_code=400, detail="Evento no encontrado")
+    except exceptions.CosmosHttpResponseError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+#Listar Proyectos GET
+@app.get("/proyectos/", response_model=List[Proyecto])
+def Listar_Proyectos():
+    query = "SELECT * FROM c"
+    items = list(container_proyecto.query_items(query=query, enable_cross_partition_query=True))
+    return items
+
+#Listar Proyectos por usuario GET
+@app.get("/proyectos/{id_usuario}", response_model=List[Proyecto])
+def Listar_Proyectos(id_usuario: str):
+    query = f"SELECT * FROM c WHERE c.id_usuario= '{id_usuario}'"
+    items = list(container_proyecto.query_items(query=query, enable_cross_partition_query=True))
+    return items
+
+#Actualizar proyecto PUT
+@app.put("/proyecto/{proyect_id}", response_model=Proyecto)
+def update_proyecto(proyect_id:str, updated_usuario: Proyecto):
+    try:
+        existing_proyecto = container_proyecto.read_item(item=proyect_id, partition_key=proyect_id)
+        existing_proyecto.update(updated_usuario.dict(exclude_unset=True))
+        container_proyecto.replace_item(item=proyect_id, body=existing_proyecto)
+
+        return existing_proyecto
+    except exceptions.CosmosResourceNotFoundError:
+        raise HTTPException(status_code=404, detail='Proyecto no encotrado')
+    except exceptions.CosmosHttpResponseError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    
+#Eliminar proyecto DELETE
+@app.delete("/proyecto/{proyect_id}", status_code=204)
+def delete_proyecto(proyect_id: str):
+    try:
+        container_proyecto.delete_item(item=proyect_id, partition_key=proyect_id)
+        return
+    except exceptions.CosmosResourceNotFoundError:
+        raise HTTPException(status_code=404, detail='Proyecto no encontrado')
+    except exceptions.CosmosHttpResponseError as e:
+        raise HTTPException(status_code=400, detail=str(e))
